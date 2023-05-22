f81615f44f5756da7ff2a70e4aecb6a86fe65241
alyhd <alyhd8080@gmail.com>
2023-05-22 14:14:27 +0330

    first commit

---
+++ android/app/src/main/kotlin/com/example/air_line/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.air_line
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+debugShowCheckedModeBanner: false,
+      theme: ThemeData.light(),
+      home: const FlightHomeApp(),
+    );
+  }
+}
+
+class FlightHomeApp extends StatefulWidget {
+  const FlightHomeApp({Key? key}) : super(key: key);
+
+  @override
+  State<FlightHomeApp> createState() => _FlightHomeAppState();
+}
+
+class _FlightHomeAppState extends State<FlightHomeApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
+
